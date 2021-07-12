7059f90b8571bb07891acb1d2a69f5953ed8fc81
Ralph Largo <devlargs@gmail.com>
2021-07-12 15:34:54 +0800

    chore: add chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import { Grid, Text } from '@chakra-ui/react';
+import NextImage from 'next/image';
 import { FC } from 'react';
 
-const Home: FC = () => {
-  return <div>Hello newbies!</div>;
-};
+const Home: FC = () => (
+  <Grid placeContent="center" h="100vh">
+    <>
+      <NextImage src="/logo.png" width={200} height={200} />
+      <Text fontSize="xl" mt="2rem" textAlign="center">
+        Welcome to HOV!
+      </Text>
+    </>
+  </Grid>
+);
 
 export default Home;
