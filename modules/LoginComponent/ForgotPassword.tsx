ce83589f4b4703715df5ce64cc7cc3ee4fc850c7
Daniel Aranas <danielaranas@gmail.com>
2021-08-06 00:58:16 +0800

    Test change

--- modules/LoginComponent/ForgotPassword.tsx
+++ modules/LoginComponent/ForgotPassword.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 const ForgotPassword: FC = () => (
   <Flex justify="flex-end">
     <Text fontSize="sm" fontWeight="bold" color="purple.400">
-      Forgot password
+      Forgot password?
     </Text>
   </Flex>
 );
