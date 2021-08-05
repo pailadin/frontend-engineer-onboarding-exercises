6ca5a0a286f466bcb90fad57008bc66179bce195
Daniel Aranas <danielaranas@gmail.com>
2021-08-06 01:01:39 +0800

    Removed leftover gh-pages line

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const inProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: inProd ? '/hov-frontend-engineer-onboarding-exercises/' : '',
 };
