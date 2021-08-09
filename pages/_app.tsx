e0d3a1b1b8105f92b9471c92e3b0f1efed67a085
Daniel Aranas <danielaranas@gmail.com>
2021-08-09 14:29:27 +0800

    Added Apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import store from '@store';
 import { AppProps } from 'next/app';
 import { FC } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <ReduxProvider store={store}>
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  </ReduxProvider>
-);
+const DEFAULT_GRAPHQL_URI = 'https://frontend-engineer-onboarding-api-thxaa.ondigitalocean.app/graphql';
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const client = new ApolloClient({
+    uri: process.env.GRAPHQL_URI || DEFAULT_GRAPHQL_URI,
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <ReduxProvider store={store}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ReduxProvider>
+    </ApolloProvider>
+  );
+};
 
 export default App;
