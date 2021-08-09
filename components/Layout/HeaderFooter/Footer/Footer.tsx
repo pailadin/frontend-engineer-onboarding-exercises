61643b0ec82771db1ae7a2671f9bf68c7e15499c
Daniel Aranas <danielaranas@gmail.com>
2021-08-09 09:44:20 +0800

    Added Dribbble icon

--- components/Layout/HeaderFooter/Footer/Footer.tsx
+++ components/Layout/HeaderFooter/Footer/Footer.tsx
@@ -1,11 +1,10 @@
 import { Box, Text } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import {
+  FaDribbble as IconDribbble,
   FaFacebook as IconFacebook,
   FaGithub as IconGithub,
   FaInstagram as IconInstagram,
-  // TODO Ask what the icon in the Figma is
-  FaQuestionCircle as IconUnknown,
   FaTwitter as IconTwitter,
 } from 'react-icons/fa';
 import Container from '../Container';
@@ -26,7 +25,7 @@ const LayoutFooter: FC = () => {
         <Icon as={IconInstagram} href="https://www.instagram.com" />
         <Icon as={IconTwitter} href="https://www.twitter.com" />
         <Icon as={IconGithub} href="https://www.github.com" />
-        <Icon as={IconUnknown} href="https://www.example.com" />
+        <Icon as={IconDribbble} href="https://dribbble.com/" />
       </Stack>
     </Container>
   );
