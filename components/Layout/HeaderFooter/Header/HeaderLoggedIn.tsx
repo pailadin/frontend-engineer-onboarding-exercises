b07e28f3f26ac6550c78ad2662e7c2714ea58fc4
Daniel Aranas <danielaranas@gmail.com>
2021-08-13 10:58:43 +0800

    Logout now clears Apollo cache

--- components/Layout/HeaderFooter/Header/HeaderLoggedIn.tsx
+++ components/Layout/HeaderFooter/Header/HeaderLoggedIn.tsx
@@ -1,32 +1,23 @@
 import { Circle, Icon, Image } from '@chakra-ui/react';
-import { useAppDispatch as useDispatch } from '@store/hooks';
-import { clearUser } from '@store/userSlice';
 import React, { FC } from 'react';
 import { FaRegBell as IconNotification } from 'react-icons/fa';
 import ItemWrapper from '../ItemWrapper';
 import ButtonLink from './ButtonLink';
 
-const HeaderLoggedIn: FC = () => {
-  const dispatch = useDispatch();
-  const logout = (): void => void dispatch(clearUser());
+const HeaderLoggedIn: FC = () => (
+  <>
+    <ItemWrapper>
+      <Icon as={IconNotification} w={6} h={6} />
+    </ItemWrapper>
 
-  return (
-    <>
-      <ItemWrapper>
-        <Icon as={IconNotification} w={6} h={6} />
-      </ItemWrapper>
+    <ItemWrapper>
+      <Circle>
+        <Image src="/default-profile-image.jpg" rounded="full" maxHeight={12} border="2px solid black" />
+      </Circle>
+    </ItemWrapper>
 
-      <ItemWrapper>
-        <Circle>
-          <Image src="/default-profile-image.jpg" rounded="full" maxHeight={12} border="2px solid black" />
-        </Circle>
-      </ItemWrapper>
-
-      <ButtonLink href="/login" onClick={logout}>
-        Logout
-      </ButtonLink>
-    </>
-  );
-};
+    <ButtonLink href="/logout">Logout</ButtonLink>
+  </>
+);
 
 export default HeaderLoggedIn;
--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,12 +1,19 @@
+import { useApolloClient } from '@apollo/client';
 import Redirect from '@components/Redirect';
 import { useAppDispatch as useDispatch } from '@store/hooks';
 import { clearUser } from '@store/userSlice';
 import { FC } from 'react';
 
 const Logout: FC = () => {
+  const client = useApolloClient();
   const dispatch = useDispatch();
 
-  return <Redirect url="/login" extraActions={(): void => void dispatch(clearUser())} />;
+  const logout = (): void => {
+    void dispatch(clearUser());
+    void client.resetStore();
+  };
+
+  return <Redirect url="/login" extraActions={logout} />;
 };
 
 export default Logout;
