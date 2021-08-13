9c462a00402de1fde69c6fd5aa70503181af22e8
Daniel Aranas <danielaranas@gmail.com>
2021-08-13 12:22:08 +0800

    Apollo now persisted, clears on logout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,76 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import store, { persistor } from '@store';
+import reduxStore, { persistor as reduxPersistor } from '@store';
+import { CachePersistor, LocalStorageWrapper } from 'apollo3-cache-persist';
 import { AppProps } from 'next/app';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const DEFAULT_GRAPHQL_URI = 'https://frontend-engineer-onboarding-api-thxaa.ondigitalocean.app/graphql';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  const headers: Record<string, string> = {};
-  const token = store.getState().user.token;
-  if (token) {
-    headers.authorization = `Bearer ${token}`;
-  }
+  const [apolloClient, setApolloClient] = useState<ApolloClient<NormalizedCacheObject>>();
+  const [apolloPersistor, setApolloPersistor] = useState<CachePersistor<NormalizedCacheObject>>();
 
-  const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URI || DEFAULT_GRAPHQL_URI,
-    cache: new InMemoryCache(),
-    headers,
-  });
+  const token = reduxStore.getState().user.token;
 
-  // https://github.com/apollographql/apollo-client/issues/2555#issuecomment-648280766:
-  client.defaultOptions = {
-    // https://github.com/apollographql/apollo-client/issues/2555#issuecomment-490866804:
-    watchQuery: {
-      fetchPolicy: 'cache-and-network',
-      errorPolicy: 'all',
-    },
-    query: {
-      errorPolicy: 'all',
-    },
+  const clearApolloPersistor = async (): Promise<boolean> => {
+    if (apolloPersistor) {
+      await apolloPersistor.purge();
+
+      if (apolloClient) {
+        await apolloClient.clearStore();
+      }
+
+      return true;
+    }
+
+    return false;
   };
 
+  useEffect(() => {
+    const init = async ({ token }): Promise<unknown> => {
+      await clearApolloPersistor();
+
+      const headers: Record<string, string> = {};
+      if (token) {
+        headers.authorization = `Bearer ${token}`;
+      }
+
+      const cache = new InMemoryCache();
+      const newPersistor = new CachePersistor({
+        cache,
+        storage: new LocalStorageWrapper(window.localStorage),
+        debug: true,
+        trigger: 'write',
+      });
+      await newPersistor.restore();
+
+      setApolloPersistor(newPersistor);
+
+      setApolloClient(
+        new ApolloClient({
+          uri: process.env.NEXT_PUBLIC_GRAPHQL_URI || DEFAULT_GRAPHQL_URI,
+          cache,
+          headers,
+        })
+      );
+
+      return;
+    };
+
+    // eslint-disable-next-line no-console
+    init({ token }).catch(console.error);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token]);
+
+  if (!apolloClient) return null;
+
   return (
-    <ReduxProvider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ApolloProvider client={client}>
+    <ReduxProvider store={reduxStore}>
+      <PersistGate loading={null} persistor={reduxPersistor}>
+        <ApolloProvider client={apolloClient}>
           <ChakraProvider>
             <Component {...pageProps} />
           </ChakraProvider>
--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,16 +1,15 @@
-import { useApolloClient } from '@apollo/client';
 import Redirect from '@components/Redirect';
 import { useAppDispatch as useDispatch } from '@store/hooks';
 import { clearUser } from '@store/userSlice';
 import { FC } from 'react';
 
 const Logout: FC = () => {
-  const client = useApolloClient();
   const dispatch = useDispatch();
 
-  const logout = (): void => {
-    void dispatch(clearUser());
-    void client.resetStore();
+  const logout = async (): Promise<unknown> => {
+    dispatch(clearUser());
+
+    return;
   };
 
   return <Redirect url="/login" extraActions={logout} />;
